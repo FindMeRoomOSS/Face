33dc5600ededd214cdb517dc00712d2f3fd365bf
Alexei Strots <alexei_strots@agilent.com>
2015-10-27 23:58:11 -0700

    request geolocation on application start

--- app/js/actions/app.js
+++ app/js/actions/app.js
@@ -1,8 +1,29 @@
 export const APP_ACTIVATE = 'activate'; // activate connection and restore user state
 export const APP_ACTIVATED = 'started'; // activate connection and restore user state
+export const APP_LOCATION = 'geolocation';
 
 export function initialize() {
-	return appActivate(); // TODO: call UserSession
+	return (dispatch) => {
+		dispatch(appActivate());
+		return getUserPosition()
+			.then((position) => dispatch(positionReceived(position)))
+			.catch((error) => console.error(error))
+	}
+}
+
+// API
+function getUserPosition() {
+	return new Promise(function(resolve, reject) {
+		navigator.geolocation.getCurrentPosition(resolve, reject);
+	});
+}
+
+function positionReceived(position) {
+	console.log(position.coords.latitude, position.coords.longitude);
+	return {
+		type: APP_LOCATION,
+		payload: position
+	};
 }
 
 function appActivate() {
